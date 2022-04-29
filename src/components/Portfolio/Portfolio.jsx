b72596878fccd394abe44a17f6df8a1d73c9fad9
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-04-29 17:51:11 +0530

    finalized

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,7 +8,26 @@ import bewakoof from "../../img/bewakoof.png";
 import Youtube from "../../img/youtube.png";
 import icoder from "../../img/icoder.png";
 import food from "../../img/food.png";
+import { motion } from "framer-motion";
 import { themeContext } from "../../Context";
+import { FaReact, FaHardHat } from "react-icons/fa";
+import {
+  SiBlockchaindotcom,
+  SiHtml5,
+  SiMaterialui,
+  SiChai,
+  SiEthereum,
+  SiExpress,
+  SiSocketdotio,
+  SiTailwindcss,
+  SiReactrouter,
+  SiJavascript,
+  SiMongodb,
+  SiNodedotjs
+} from "react-icons/si";
+import { DiCss3 } from "react-icons/di";
+import { VscJson } from "react-icons/vsc";
+import { IoLogoJavascript } from "react-icons/io";
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
@@ -19,31 +38,271 @@ const Portfolio = () => {
       <span>Portfolio</span>
 
       {/* slider */}
-      <Swiper
-        spaceBetween={30}
-        slidesPerView={3}
-        grabCursor={true}
-        className="portfolio-slider"
-      >
-        <SwiperSlide>
-          <a href="https://astonishing-stardust-e63665.netlify.app/" rel="noreferrer" target={"_blank"}><img src={Youtube} alt="" style={{height: "200px"}}/></a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="https://sk-myntra.netlify.app/" rel="noreferrer" target={"_blank"}><img src={myntra} alt="" style={{height: "200px"}}/></a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="https://sleepy-neumann-26057b.netlify.app/" rel="noreferrer" target={"_blank"}><img src={food} alt="" style={{height: "200px"}}/></a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="https://zealous-brattain-f33b87.netlify.app/" rel="noreferrer" target={"_blank"}><img src={icoder} alt="" style={{height: "200px"}}/></a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="https://copybewakoof.netlify.app/" rel="noreferrer" target={"_blank"}><img src={bewakoof} alt="" style={{height: "200px"}}/></a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href=""><img src={news} alt="" style={{height: "200px"}}/></a>
-        </SwiperSlide>
-      </Swiper>
+    
+      <div className="section">
+       
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img
+                  src={Youtube}
+                  alt="Youtube"
+                />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>Youtube</h2>
+              <p>
+                Clone of the popular entertainment video site Youtube. Built on HTML , CSS for front end and used official youtube API. It's an individual project executed in 1 day.
+              </p>
+              <div>
+                
+                <SiHtml5 />
+                <IoLogoJavascript />
+               
+              </div>
+              <div>
+                <a
+                  href="https://astonishing-stardust-e63665.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    See this Live
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/skbiswal01/Youtube-clone-API"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img
+                  src={myntra}
+                  alt="Myntra"
+                />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>Myntra Clone</h2>
+              <p>
+                First Backend Project with NodeJS, ExpressJS and MongoDB. Used EJS to render views. Main functionalities were : Register/Login using JWT, Sorting and Filtering in the backend, Cart and Wish List and many more.
+              </p>
+              <div>
+                <IoLogoJavascript />
+                <FaReact />
+                <SiNodedotjs />
+                <SiExpress />
+                <SiMongodb />
+              </div>
+              <div>
+                <a
+                  href="https://sk-myntra.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    See this Live
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/skbiswal01/Mynta-Full-Stack-Clone"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img
+                  src={food}
+                  alt="food website"
+                />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>food website</h2>
+              <p>
+              A solo project on providing information of food receipe. Backend API for fetching data  
+              </p>
+              <div>
+                <SiTailwindcss />
+                <SiHtml5 />
+                <SiJavascript />
+              </div>
+              <div>
+                <a
+                  href="sleepy-neumann-26057b.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    URL for the API
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/skbiswal01/Food-recipe-project"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img
+                  src={icoder}
+                  alt="icoder"
+                />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>icoder</h2>
+              <p>
+                Mini project of A classic blog website built using bootstrap. 
+              </p>
+              <div>
+                <SiJavascript />
+                <SiHtml5 />
+                <DiCss3 />
+              </div>
+              <div>
+                <a
+                  href="https://zealous-brattain-f33b87.netlify.app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    See this Live
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/skbiswal01/Blog-site-using-Bootstrap"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img
+                  src={bewakoof}
+                  alt="bewakoof"
+                />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>Bewakoof.com clone</h2>
+              <p>
+                A project on famous printed articles T-shirt e-commerce website build using simple tech stack executed in 5 days;
+              </p>
+              <div>
+                <SiHtml5 />
+                <DiCss3 />
+                <IoLogoJavascript />
+              </div>
+              <div>
+                <a
+                  href="https://cloneofbewakoof.netlify.app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    See this Live
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/AmbujKrAK/Bewakoof.com--Project-"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+     
+         <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img
+                  src={news}
+                  alt="news site"
+                />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>News website</h2>
+              <p>
+                my first project build using react Js and using API server calls build in 2 days
+              </p>
+              <div>
+                <SiReactrouter />
+
+              </div>
+              <div>
+                <a
+                  href="https://imaginative-sherbet-909200.netlify.app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    See this live
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/skbiswal01/news-app-react-project"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button type="button" className="button i-button">
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
